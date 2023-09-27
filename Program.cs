6d857c647c490dbec78a1b528fa1c2d3e961d481
Снежок <144549734+Shizofrenia999@users.noreply.github.com>
2023-09-27 17:27:27 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,34 +1,46 @@
 ﻿using System;
 
-namespace PZ_01
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Random random = new Random();
+        int[] array = new int[10];
+        int count = 0;
+
+        for (int i = 0; i < array.Length; i++)
         {
-            Console.WriteLine("введите число А: ");
-            double a= double.Parse(Console.ReadLine()); //ввод первого числа (а)
+            array[i] = random.Next(20, 101);
+            if (array[i] % 10 == 0)
+            {
+                count++;
+            }
+        }
 
-            Console.WriteLine("введите число В: ");
-            double b = double.Parse(Console.ReadLine()); //ввод второго числа (b)
+        int[] newArray = new int[count];
+        int index = 0;
 
-            Console.WriteLine("введите число С: ");
-            double c = double.Parse(Console.ReadLine()); //ввод третьего числа (c)
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 10 == 0)
+            {
+                newArray[index] = array[i];
+                index++;
+            }
+        }
 
-            double result1, result2, result3, result4, result5, result6, result7, result8; //переменные для решения по частям
+        Console.WriteLine("Исходный массив:");
+        foreach (int number in array)
+        {
+            Console.Write(number + " ");
+        }
 
-            result1 = 3 * (Math.Log(8, a)); //1 действие (1 дробь, числитель)
-            result2 = Math.Sin(b / (3 * a)); //2 действие (1 дробь, знаменатель)
-            result3 = result1 / result2; //3 действие (ответ 1ой дроби)
-            result4 = Math.Pow(((5 * Math.Pow(a, 2))+7), (1 / 3.0)); //4 действие (вычисляем корень 3ей степени) 
-            result5 = 4 * (Math.Abs(c - 2 * a + 1));//5 действие (числитель второй дроби, с модулем)
-            result6 = Math.Sqrt(8 * a);//6 действие (знаменатель 2ой дроби. Квадратный корень)
-            result7 = result5 / result6;//7 действие (ответ второй дроби)
-            result8 = result3 - result4 + result7; //получаем конечный ответ
+        Console.WriteLine();
 
-            Console.WriteLine($"Ответ: { result8} "); //вывод ответа
-
-
+        Console.WriteLine("Массив чисел, оканчивающихся на 0:");
+        foreach (int number in newArray)
+        {
+            Console.Write(number + " ");
         }
     }
 }
