5ebdb91642cc5fab83e49be860a5ed8b4dc83a6a
Снежок <144549734+Shizofrenia999@users.noreply.github.com>
2023-12-22 18:53:59 +0500

    Add files via upload

---
+++ Pz_10.cs.cs
@@ -0,0 +1,40 @@
+﻿namespace pz10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите несколько строк(для завершения введите пустую строку):");
+
+            List<string> inputStrings = new List<string>();
+            String input;
+            do
+            {
+                input = Console.ReadLine();
+                if(!string.IsNullOrWhiteSpace(input))
+                {
+                    inputStrings.Add(input);
+                }
+            } 
+            while (!string.IsNullOrWhiteSpace(input)) ;
+
+            String concatenatedString = String.Join(" ", inputStrings); // конкатенация строк
+            String[] words = concatenatedString.Split(' '); // разделение строки на слова
+
+            List<string> uniqueWords = new List<string>();
+            // Проверка на уникальность слов и их удаление, если они повторяются
+            foreach(string word in words)
+            {
+                if(!uniqueWords.Contains(word))
+                {
+                    uniqueWords.Add(word);
+                }
+            }
+            String resultString = String.Join(" ", uniqueWords); // объединение уникальных слов в строку
+            Console.WriteLine("Результат после конкатенации и удаления повторяющихся слов:");
+            Console.WriteLine(resultString);
+
+        }
+    }
+}
